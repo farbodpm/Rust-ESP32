c9f6efb08398fd2ef1f6f3dfc0a86ccc4c999e21
FarbodPM <farbod.peimanzadeh@gmail.com>
2022-06-21 09:49:18 +0430

    add to bashrc

---
+++ add_to_bashrc.sh
@@ -0,0 +1,3 @@
+export PATH="/home/$USER/.espressif/tools/xtensa-esp32-elf-clang/esp-14.0.0-20220415-x86_64-unknown-linux-gnu/bin/:/home/morteza/.espressif/tools/xtensa-esp32-elf-gcc/8_4_0-esp-2021r2-patch3-x86_64-unknown-linux-gnu/bin/:$PATH"
+export LIBCLANG_PATH="/home/$USER/.espressif/tools/xtensa-esp32-elf-clang/esp-14.0.0-20220415-x86_64-unknown-linux-gnu/lib/"
+export PIP_USER="no"
